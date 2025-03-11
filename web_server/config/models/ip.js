231c8d1f3a21026e0f59a43169082a4027d7a20e
Mobb autofixer <git@mobb.ai>
2025-03-11 19:13:10 +0000

    mobb fix commit: cb2df935-b33f-47b5-9ad2-3aca1bf73689

--- web_server/config/models/ip.js
+++ web_server/config/models/ip.js
@@ -12,6 +12,7 @@
  * governing permissions and limitations under the License.
  */
 
+const mongoSanitize = require('express-mongo-sanitize');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -88,7 +89,7 @@ module.exports = {
             }).countDocuments().exec();
         } else {
             return allIPsModel.find({
-                'domains': domain,
+                'domains': mongoSanitize.sanitize({ data: domain }).data,
             }).exec();
         }
     },
